22a6c816305a8cad29260779f02765ef3c621d19
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-07-31 12:54:26 +0530

    added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { weatherApi } from '../weatherApi';
 import { useSelector } from 'react-redux';
 import { FaNewspaper } from 'react-icons/fa';
 import axios from 'axios';
@@ -14,15 +13,24 @@ const WeatherNews = () => {
 
   useEffect(() => {
     const fetchWeatherNews = async () => {
+      const loadingToast = toast.loading('Searching for news...');
       try {
-        setLoading(true);
-        const loadingToast = toast.loading('Searching for weather...')
-        const newsData = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${import.meta.env.VITE_NEWSKEY}`)
-        console.log(newsData.data)
-        setNews(newsData.data);
-            toast.dismiss(loadingToast)
+        const response = await axios.get(
+          `https://api.currentsapi.services/v1/search`, 
+          {
+            params: {
+              apiKey: import.meta.env.VITE_NEWSKEY,
+              keywords: 'weather',
+              language: 'en'
+            }
+          }
+        );
+        setNews(response.data.news);
+        toast.dismiss(loadingToast);
       } catch (error) {
         setError('Failed to fetch weather news.');
+        toast.error('Failed to fetch weather news.');
+        toast.dismiss(loadingToast);
       } finally {
         setLoading(false);
       }
@@ -32,22 +40,21 @@ const WeatherNews = () => {
   }, []);
 
   if (loading) {
-    return <p className='hidden'>Loading news...</p>;
+    return <p className="hidden">Loading news...</p>;
   }
 
   if (error) {
     return <p>{error}</p>;
   }
-  console.log(news)
 
   return (
-    <div className={`p-4 ${theme === 'light' ? 'bg-black text-white':'bg-white text-black' } w-[70%] rounded-xl mt-8`}>
+    <div className={`p-4 ${theme === 'light' ? 'bg-black text-white' : 'bg-white text-black'} w-[70%] rounded-xl mt-8`}>
       <h2 className="text-2xl font-bold flex items-center">
         <FaNewspaper className="mr-2" /> News
       </h2>
-      <div className="mt-4 ]">
-        {news.articles.length > 0 ? (
-          news.articles.map((article, index) => (
+      <div className="mt-4">
+        {news.length > 0 ? (
+          news.map((article, index) => (
             <div key={index} className="border-b-2 py-2">
               <h3 className="text-xl font-semibold">{article.title}</h3>
               <p>{article.description}</p>
